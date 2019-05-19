a927ed3a5b0e6c7beaf46e58b9013cb0bbd2f915
lazywriter <yash.w.thakur@gmail.com>
2019-05-19 21:51:14 +0530

    Updated Version

---
+++ data/books.js
@@ -0,0 +1,55 @@
+let booksJson = [{
+        bookReviewDate: "19/05/2019",
+        bookReviewedBy: "Yashwardhan Thakur",
+        bookName: "The Da Vinci Code",
+        bookAuthor: "Dan Brown",
+        bookImage: "./img/book-da-vinci.jpg",
+        bookAuthorImage: "./img/writer-dan-brown.jpg",
+        bookSubTitle: "Book Review",
+        bookDescription: "An amazing thriller novel written by the famous author Dan Brown. One of the novels featuring proffesor Robert Langdon.",
+        bookIntroduction: 'Thwe Da Vinci Code was released in 2003.It is a mystery thrillerwhich follows "symbologist" Robert Langdon and cryptologist Sophie Neveu after a murder in the Louvre Museum in Paris that causes them to become involved in a battle between two christian orgnizations that suggest an answer to an old age mystery that stretches deep into the vaults of history.',
+        bookPlotSummary: 'the story begins with the assassination of The curator of Louvre Mueseum and the priory of Sion grand master Jacques sauniere by an albino catholic monk named Silas. \
+        After Sauniere\'s body is discoverd in the pose of the Vitruvian Man, the police summons Harvard proffesor Robert Langdon, who is in town for business. the police captain teels him that he was summoned here to decode the cryptic message Sauniere left before dying. \
+        Sophie Neveu, a police cryptographer who is the estranded grandaughter of Sauniere explains to Langdon that Bezu Fache , the police captain thinks that Langdon murdered Sauniere because the last line of her grandfather\'s message said,"P.S Find Robert Langdon" which Fache erased prior to lLangdon\'s arrival! \
+        Neveu and Langdon escape the police and visit hte depository bank of Zurich for a safe deposit box. in the safe deposit box they find: a cryptex(a cylindrical vault with five rotating dials labelled with letters) and clues to its password. \
+        Langdon and Neveu take the cryptex to Sir Leigh Teabing, a friend of Langdon\'s who is an expert on the Holy grail. Teabing explains that the Holy Grail is not a cup, but a tomb containing the bones of Mary Magdalene(One of the twelve followers of Jesus). the trio then flees the country on Teabing\'s private plane. They conclude that the proper combination of the letters spell out \'Sophia\', the name given to Neveu. the cryptex opens revealing another small cryptex inside it! \
+        by hte time they arrive at Westminster Abbey, Teabing is reveled to be the master of Silas. Teabing wishes to use the Holy grail to ruin the vatican. he forces Langdon to open the cryptex at gunpoint to solve the cryptex password , which Langdo nrealizes is \apple\'. Landon secretly opens the cryptex and throws it after taking the contents. Teabing is alter arrested by Bezu Fache,the police captain who realizes that Langdon is innocent. \
+        the final message of the keystone trakes Langs=don and Neveu to Rosslyn Chapel whose docents turn out to be Neveu,s brother and grandmother who she beleived died in a car accident. Neveu also finds that she is the descendant of Jesus Christ and Mary Magdalene Landon also finds out that the tomb of Mary magdalene is buried beneath the pyramid in the Louvre mueseum.',
+        bookPersonalReview: 'Like every other Robert Brown novel, The Da Vinci code stood up to its mark. the book was very interesting and it is impossible to keep down the book after starting it. The book provides a detailed narrative and surprises at every end. But it is not my most favourite Robert Brown novel. However, I would recommend yopu tjis book if you are looking for a graet thriller novel.',
+        bookRating: "8.4",
+        bookPageUrl: "./pages/the-da-vinci-code.html"
+    },
+    {
+        bookReviewDate: "19/05/2019",
+        bookReviewedBy: "Yashwardhan Thakur",
+        bookName: "Angels and Demons",
+        bookAuthor: "",
+        bookImage: "./img/book-angels-demons.jpg",
+        bookAuthorImage: "./img/writer-dan-brown.jpg",
+        bookSubTitle: "Book Review",
+        bookDescription: 'Angels and Demons is a superb mystery thriller novel written by the author Dan Brown. It is the first novel of the Robert Langdon series',
+        bookIntroduction: "",
+        bookPlotSummary: "",
+        bookPersonalReview: "",
+        bookRating: "8.4",
+        bookPageUrl: "./pages/angels-and-demons.html"
+    },
+    {
+        bookReviewDate: "19/05/2019",
+        bookReviewedBy: "Yashwardhan Thakur",
+        bookName: "Sacred Games",
+        bookAuthor: "",
+        bookImage: "./img/book-sacred-games.jpg",
+        bookAuthorImage: "./img/writer-vikram-chandra.jpg",
+        bookSubTitle: "Book Review",
+        bookDescription: "A novel by Vikram Chandra based on the the victorian India. the novel takes us into the criminal world of Bombay. \
+        the story follows...",
+        bookIntroduction: "",
+        bookPlotSummary: "",
+        bookPersonalReview: "",
+        bookRating: "8.4",
+        bookPageUrl: "./pages/sacred-games.html"
+    }
+];
+
+var booksData = JSON.stringify(booksJson);
